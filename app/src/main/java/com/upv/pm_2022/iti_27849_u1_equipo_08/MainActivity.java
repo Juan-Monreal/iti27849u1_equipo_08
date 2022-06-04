400761705a45ef263c8a85920b6c124e163c8f5d
jmonrealr <jcmonrealr@gmail.com>
2022-06-04 18:24:10 -0500

    Initial Project

---
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_08/MainActivity.java
@@ -0,0 +1,14 @@
+package com.upv.pm_2022.iti_27849_u1_equipo_08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
