2bc074a58856c8b7774ca1cae7d900d4aeb0a9a9
Crxwler <loco7139@gmail.com>
2022-06-04 19:00:48 -0500

    add: Customer Class

---
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_08/Customer.java
@@ -0,0 +1,52 @@
+package com.upv.pm_2022.iti_27849_u1_equipo_08;
+
+import androidx.annotation.NonNull;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String last_name;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name, String last_name) {
+        this.id = id;
+        this.name = name;
+        this.last_name = last_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                '}';
+    }
+}
