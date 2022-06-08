2deb151c77bb0f84ae48ef75c693c7c2e0499607
Crxwler <loco7139@gmail.com>
2022-06-08 18:23:02 -0500

    add: OwnerController (DB Handling)

---
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_08/controllers/OwnerController.java
@@ -0,0 +1,49 @@
+package com.upv.pm_2022.iti_27849_u1_equipo_08.controllers;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.upv.pm_2022.iti_27849_u1_equipo_08.DbHandler;
+import com.upv.pm_2022.iti_27849_u1_equipo_08.Owner;
+
+public class OwnerController {
+
+    private final String TABLE_NAME = "Owners";
+    private SQLiteDatabase db;
+
+    OwnerController(SQLiteDatabase db){
+        this.db = db;
+    }
+
+    void addOwner(Owner owner){
+        ContentValues values = new ContentValues();
+        values.put("name", owner.getName());
+        values.put("last_name", owner.getLast_name());
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+
+    Owner getOwner(int id){
+        Owner owner;
+        Cursor cursor = db.query(TABLE_NAME,
+                new String[] { "id", "name", "last_name"}, "id+?",
+                new String[] { String.valueOf(id)},
+                null, null, null, null);
+        if (cursor != null){
+            cursor.moveToFirst();
+            owner = new Owner(Integer.parseInt(cursor.getString(0)),
+                    cursor.getString(1),
+                    cursor.getString(2));
+        }else
+            owner = null;
+        return owner;
+    }
+    public  int updateOwner(Owner owner){
+        ContentValues values = new ContentValues();
+        values.put("name", owner.getName());
+        values.put("last_name", owner.getLast_name());
+        return db.update(TABLE_NAME, values, "id = ?",
+                new String[] { String.valueOf(owner.getId())});
+    }
+}
